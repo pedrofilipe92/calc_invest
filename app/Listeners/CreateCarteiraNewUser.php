4e36824ecf94fb180a31656e0fddff149deb1d33
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-08-17 20:31:40 -0300

    criando carteira automaticamente

---
+++ app/Listeners/CreateCarteiraNewUser.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Carteira;
+
+class CreateCarteiraNewUser
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(UserCreated $event)
+    {
+        Carteira::create([
+            'user_id'               => $event->user->id,
+            'qtd_investimentos'     => 0,
+            'total_aplicado'        => 0
+        ]);
+    }
+}

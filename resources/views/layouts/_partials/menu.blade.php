ed547569ce059dc24e9fa06e8006e864b3f1ec78
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-08-24 13:46:24 -0300

    implementa user seeder

--- resources/views/layouts/_partials/menu.blade.php
+++ resources/views/layouts/_partials/menu.blade.php
@@ -1,6 +1,8 @@
-<li>
-    <a class="navbar-brand" href="{{ route('carteira.index') }}">Minha Carteira</a>
-</li>
+@if (auth()->user()->name != 'admin')
+    <li>
+        <a class="navbar-brand" href="{{ route('carteira.index') }}">Minha Carteira</a>
+    </li>
+@endif
 <li>
     <a class="navbar-brand" href="{{ route('investimento.index') }}">Investimentos</a>
 </li>
